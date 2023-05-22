554fcff642eac8ab390f16f4e43ed63d2be964c4
Miguel Jesus <miguel.alfonsosanchez9@gmail.com>
2023-05-22 13:32:14 +0200

    seguridad implementada

--- src/main/java/com/example/InmoCarlosIII/DTO/UsuarioDTO.java
+++ src/main/java/com/example/InmoCarlosIII/DTO/UsuarioDTO.java
@@ -17,45 +17,9 @@ public class UsuarioDTO {
 
     private String clave;
 
+    private String usuario;
+
     private String telefono;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
---
+++ src/main/java/com/example/InmoCarlosIII/security/UserDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.InmoCarlosIII.security;
+
+import com.example.InmoCarlosIII.Entities.Usuario;
+import com.example.InmoCarlosIII.Repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepositorio;
+    @Override
+    public UserDetails loadUserByUsername(String usuario) throws UsernameNotFoundException {
+        Usuario usuario1 = usuarioRepositorio
+                .findOneByUsuario(usuario)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario con user " + usuario + " no ha sido encontrado"));
+        return new UserDetailsImpl(usuario1);
+    }
+
+
+}
